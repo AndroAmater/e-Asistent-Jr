12aded68fdf42922a5cd17488efb32388a39f399
AndroAmater <andrejfidel@Andrejs-MacBook-Air.local>
2015-06-30 10:19:32 +0200

    General cleanup and working HTML rendering

--- flask-run.py
+++ flask-run.py
@@ -2,9 +2,11 @@
 from flask import *
 app = Flask(__name__)
 
-@app.route("/")
+app.debug = True
+
+@app.route('/')
 def hello():
-	return "Hello World!"
+	return render_template('layout.html')
 
 if __name__ == "__main__":
 	app.debug = True
